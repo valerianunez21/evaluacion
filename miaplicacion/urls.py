dfd6757e6af151c089e1332225e2c880c7efc5d5
valerianunez21 <valerianunez212005@gmail.com>
2024-07-11 14:03:52 -0500

    rutas

--- miaplicacion/urls.py
+++ miaplicacion/urls.py
@@ -1,9 +1,10 @@
-from django .urls import path
+from django.urls import path
 from . import views
-urlpatterns = [    
-    path('', views.inicio, name='inicio' ),
-    path('aprendiz/', views.aprendices, name= 'aprendiz'),
-    path('formulario/', views.formulario, name='formulario' ),
-    path('eliminar/<int:idaprendiz>/', views.eliminar, name='eliminar' ),
-    path('modificar/<int:idaprendiz>/', views.modificar, name="modificar")
-    ]
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('aprendiz/', views.aprendices, name='aprendiz'),
+    path('formulario/', views.formulario, name='formulario'),
+    path('eliminar/<int:idaprendiz>/', views.eliminar, name='eliminar'),
+    path('modificar/<int:idaprendiz>/', views.modificar, name='modificar'),
+]
